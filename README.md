b8bbd4cb94bd71cc7d5479feec66770d48915ca8
stringlapse <24516915+stringlapse@users.noreply.github.com>
2019-03-01 22:32:17 -0700

    Add basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lapser
+
+##Setup
+
+Requirements:
+
+* Python 3.X - can be downloaded from https://www.python.org
+
+* The `ffmpeg-python` module - can be installed with
+```pip3 install ffmpeg-python```
+
+* FFmpeg - can be downloaded from https://www.ffmpeg.org (Note: FFmpeg must be in the system path)
---
+++ lapser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import ffmpeg
+
+input_files = input("Input files: ")
+output_file = input("Output file: ")
+width = input("Width: ")
+height = input("Height: ")
+display_framerate = input("Timelapse framerate: ")
+file_framerate = input("File framerate: ")
+scaling = '{}x{}'.format(width, height)
+stream = ffmpeg.input(input_files, pattern_type='glob')
+stream = ffmpeg.filter_(stream, 'format', 'yuv420p')
+stream = ffmpeg.filter_(stream, 'scale', scaling)
+stream = ffmpeg.output(stream, output_file)
+ffmpeg.run(stream)
